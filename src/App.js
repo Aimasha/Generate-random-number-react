d31cd7d47f7b705e924d1de93f973f52f9e53b06
AimanaTazhibaeva <Aimasha200300@gmail.com>
2022-06-28 15:30:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from  "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App (){
+
+ const [lowerLimit,setLowerLimit] = useState("")
+ const [upperLimit,setUpperLimit] = useState("")
+const  [generateNum, setGenerateNum] = useState(0)
+
+   const handleGenerate =(e)=>{
+     const min = Number(lowerLimit)
+     const max = Number(upperLimit)
+
+     if(max > min) {
+      const number = Math.floor(Math.random() * (max-min)  + min)
+      setGenerateNum(number)
+     }
+   }
+
+
+   const handleLowInput = (e) => {
+     const lowValue = e.target.value
+     setLowerLimit(lowValue)
+     console.log("lower",typeof(lowValue))
+
+   }
+
+   const handleUpInput = (e) => {
+    const UpValue = e.target.value
+    setUpperLimit(UpValue)
+    console.log("upper",typeof(UpValue))
+
+   }
+ 
+   const handleClear = () =>{
+      setGenerateNum(0) 
+      setUpperLimit(0)
+      setLowerLimit(0)
+  }
+
+  return(
+    <div>
+      <h3  className="App">Create a project to generate a random number.</h3>
+       <h1>{generateNum}</h1>
+
+       <div className="main">
+     <label htmlFor="">Lower</label>
+     <input onChange={handleLowInput} type={lowerLimit} />
+  
+     <label htmlFor="">Upper</label>
+    <input onChange={handleUpInput}  type={upperLimit}/>
+    
+       </div>
+       <div className='btn'>
+       <button onClick={handleGenerate} className='green'>Generate</button>
+       <button onClick={handleClear} className='gray'>Clear</button>
+       </div>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
